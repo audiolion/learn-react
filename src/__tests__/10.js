9fa15d799fd26e097fac65a345510b96f0711e16
Ryan Castner <castner.rr@gmail.com>
2019-04-03 17:20:03 -0400

    Exercise 11

--- src/__tests__/10.js
+++ src/__tests__/10.js
@@ -55,8 +55,8 @@ test('can play a game of tic tac toe', () => {
 /*
 http://ws.kcd.im/?ws=learn%20react&e=10&em=rcastner@kimmel.com
 */
-test.skip('I submitted my elaboration and feedback', () => {
-  const submitted = false // change this when you've submitted!
+test('I submitted my elaboration and feedback', () => {
+  const submitted = true // change this when you've submitted!
   expect(submitted).toBe(true)
 })
 ////////////////////////////////
--- src/__tests__/11.js
+++ src/__tests__/11.js
@@ -17,8 +17,8 @@ test('calls VanillaTilt.init with the root node', () => {
 /*
 http://ws.kcd.im/?ws=learn%20react&e=11&em=rcastner@kimmel.com
 */
-test.skip('I submitted my elaboration and feedback', () => {
-  const submitted = false // change this when you've submitted!
+test('I submitted my elaboration and feedback', () => {
+  const submitted = true // change this when you've submitted!
   expect(submitted).toBe(true)
 })
 ////////////////////////////////
